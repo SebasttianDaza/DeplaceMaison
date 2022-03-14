f59ff85c52bd680d2c1a93bd58fd10a28d68b4eb
Sebastian Daza <jsebastiandaza2002@gmail.com>
2022-03-14 17:04:09 -0500

    Header and Nav completed
    I've completed two components with style CSS

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -16,13 +16,19 @@ const Header = () => {
         </div>
       </section>
       <section className="contentImage">
-        <div>
+        <div className="imageClothes">
           <img
             src="https://firebasestorage.googleapis.com/v0/b/react-portfolio-863d8.appspot.com/o/secondImage.jpeg?alt=media&token=e5435af3-9b4b-463e-8b35-713272b28f2a"
             alt="avatar"
           />
         </div>
-        <div>
+        <div className="imageClothesPosition">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/deplacemaison-2f33d.appspot.com/o/imageIndex.svg?alt=media&token=0ad451ce-fd34-4550-9860-06a1499e256f"
+            alt="Image extra"
+          />
+        </div>
+        <div className="imageClothes">
           <img
             src="https://firebasestorage.googleapis.com/v0/b/react-portfolio-863d8.appspot.com/o/fitrstImage.jpeg?alt=media&token=f55663e1-fbd9-4aa5-b200-04bd1a2b5fbd"
             alt="avatar"
--- src/Components/Nav/nav.jsx
+++ src/Components/Nav/nav.jsx
@@ -1,39 +1,38 @@
 import "./nav.css";
-
+import {BsGripHorizontal} from "react-icons/bs";
 
 const Nav = () => {
-    return (
-      <nav className="Nav">
-        <section className="titleMain">
-          <div className="titlePrincipal">
-            <div className="sawImage">
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/react-portfolio-863d8.appspot.com/o/log.svg?alt=media&token=2f336c3d-a366-4bd9-be0d-e635970d7822"
-                alt="logo"
-              />
-            </div>
-  
+  return (
+    <nav className="Nav">
+      <section className="titleMain">
+        <div className="titlePrincipal">
+          <div className="sawImage">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/react-portfolio-863d8.appspot.com/o/log.svg?alt=media&token=2f336c3d-a366-4bd9-be0d-e635970d7822"
+              alt="logo"
+            />
           </div>
+        </div>
 
-          <div className="menu">
-            <ul>
-              <li>SHOP</li>
-              <li>COLLECTIONS</li>
-              <li>ABOUT</li>
-            </ul>
-          </div>
-        </section>
-        <section className="titleNav">
-          <h3>WK MODE</h3>
-          <div className="btnMenu">
-            <h1>DEPLACE MAISON</h1>
-          </div>
-        </section>
-        <section className="cartNav">
-          <p> Cart 0</p>
-        </section>
-      </nav>
-    );
-}
+        <div className="menu">
+          <ul>
+            <li>SHOP</li>
+            <li>COLLECTIONS</li>
+            <li>ABOUT</li>
+          </ul>
+        </div>
+      </section>
+      <section className="titleNav">
+        <h3>WK MODE</h3>
+        <div className="btnMenu">
+          <BsGripHorizontal />
+        </div>
+      </section>
+      <section className="cartNav">
+        <p> Cart 0</p>
+      </section>
+    </nav>
+  );
+};
 
 export default Nav;
