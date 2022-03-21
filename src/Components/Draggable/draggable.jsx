941650dd71b83a47af04a92ca7915c37001c2d5f
Sebastian Daza <jsebastiandaza2002@gmail.com>
2022-03-21 17:23:38 -0500

    Complete function draggable without responsive
    I've completed function draggable for phone and web and miss some errors

--- src/Components/Draggable/draggable.jsx
+++ src/Components/Draggable/draggable.jsx
@@ -4,30 +4,69 @@ import React, {useState, useEffect} from "react";
 
 function useLocalState(defaultValue = 0) {
   const [state, setState] = useState(defaultValue);
-
   return [state, setState];
 }
 
-function changeStyle(e, draggable = "") {
-  e.target.classList.add(draggable);
-}
-
-function removeStyle(e, draggable = "") {
-  e.target.classList.remove(draggable);
-}
-
 function Dragable() {
+  const [pressed, setPressed] = useLocalState(false);
+  const [startPosition, setStartPosition] = useLocalState(0);
+  const [Xposition, setXposition] = useLocalState(0);
   const [animate, setAnimate] = useLocalState(false);
 
   const slider = React.createRef();
   const sliderChild = React.createRef();
+
+  const startSlider = (e) => {
+    if (e.type === "mousedown") {
+      setPressed(true);
+      setStartPosition(e.clientX - slider.current.offsetLeft);
+      slider.current.style.cursor = "grabbing";
+    }
+    if (e.type === "touchstart") {
+      setPressed(true);
+      setStartPosition(e.touches[0].clientX - slider.current.offsetLeft);
+    }
+  };
+
+  const mouseTouchEnter = () => {
+    setPressed(false);
+    slider.current.style.cursor = "grab";
+  };
+
+  const moveSlider = (e) => {
+    if (!pressed) return;
+    e.preventDefault();
+    if (e.type === "mousemove") {
+      setXposition(e.clientX - startPosition);
+    }
+    if (e.type === "touchmove") {
+      setXposition(e.touches[0].clientX - startPosition);
+    }
+    sliderChild.current.style.left = Xposition + "px";
+    checkSize();
+  };
+
+  const checkSize = () => {
+    let outer = slider.current.getBoundingClientRect();
+    let inner = sliderChild.current.getBoundingClientRect();
+    if (parseInt(sliderChild.current.style.left) > 0) {
+      sliderChild.current.style.left = 0 + "px";
+    } else if (inner.right < outer.right) {
+      sliderChild.current.style.left = `-${outer.width - inner.width}px`;
+    }
+  };
 
   return (
     <div
       className="draggableSlider"
       ref={slider}
       onMouseDown={(e) => startSlider(e)}
-      onMouseMove={(e) => startMove(e)}
+      onTouchStart={(e) => startSlider(e)}
+      onMouseEnter={() => mouseTouchEnter()}
+      onMouseUp={() => mouseTouchEnter()}
+      onTouchEnd={() => mouseTouchEnter()}
+      onMouseMove={(e) => moveSlider(e)}
+      onTouchMove={(e) => moveSlider(e)}
     >
       <section className="slider" ref={sliderChild}>
         <Card
