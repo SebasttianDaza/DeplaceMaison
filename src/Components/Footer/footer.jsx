28930e15876d3535e2963cd268ffd5469f06d706
Sebastian Daza <jsebastiandaza2002@gmail.com>
2022-03-28 08:45:15 -0500

    Section email of the component Footer
    I'm working section email with the component footer

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -1,47 +1,54 @@
 import "./footer.css";
+import {ErrorBoundary} from "react-error-boundary";
+
+import ErrorFallback from "../../Errors/ErrorFallback.jsx";
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <section className="sectionEmail">
-        <div>
-          <p>suscribe to newsletter</p>
-          <img src="https://firebasestorage.googleapis.com/v0/b/deplacemaison-2f33d.appspot.com/o/imageEmail.svg?alt=media&token=0921b593-adcd-442e-950f-d805559d9e37" />
-        </div>
-        <div>
-          <form>
-            <label />
-            <div>
-              <input type="email" placeholder="email address" />
-              <input type="submit" value="SUBMIT" />
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <footer className="footer">
+        <section className="sectionEmail">
+          <div className="emailInfoContent">
+            <p>suscribe to newsletter</p>
+            <div className="emailImage">
+              <img src="https://firebasestorage.googleapis.com/v0/b/deplacemaison-2f33d.appspot.com/o/imageEmail.svg?alt=media&token=0921b593-adcd-442e-950f-d805559d9e37" />
             </div>
-          </form>
-        </div>
-      </section>
-      <section>
-        <div>
-          <h6>info</h6>
-          <ul>
-            <li>faq</li>
-            <li>returns</li>
-            <li>contact</li>
-          </ul>
+          </div>
+          <div className="emailFormContent">
+            <form className="formContent">
+              <label htmlFor="email">Subsribe to our newsletter</label>
+              <div className="contentInput">
+                <input type="email" id="email" placeholder="email address" />
+                <input type="submit" value="SUBMIT" />
+              </div>
+            </form>
+          </div>
+        </section>
+        <section>
+          <div>
+            <h6>info</h6>
+            <ul>
+              <li>faq</li>
+              <li>returns</li>
+              <li>contact</li>
+            </ul>
 
-          <p>Credits</p>
-        </div>
-        <div>
-          <h6>info</h6>
-          <ul>
-            <li>faq</li>
-            <li>returns</li>
-            <li>contact</li>
-          </ul>
+            <p>Credits</p>
+          </div>
+          <div>
+            <h6>info</h6>
+            <ul>
+              <li>faq</li>
+              <li>returns</li>
+              <li>contact</li>
+            </ul>
 
-          <p>Credits</p>
-        </div>
-      </section>
-      <section>Hola</section>
-    </footer>
+            <p>Credits</p>
+          </div>
+        </section>
+        <section>Hola</section>
+      </footer>
+    </ErrorBoundary>
   );
 };
 
